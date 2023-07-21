dac8ab2638304902fe6abfa47d5560f11d3fb50c
Reneh7 <renehm2001@gmail.com>
2023-07-21 22:57:56 +0300

    reneh-home-route-done

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -1,8 +1,29 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
+const { API_URL } = require("../config");
+
+// HOME ('/')
+Router.get("/", async (req, res, next) => {
+  try {
+    let artPieces = await axios.get(API_URL);
+    let artData = artPieces.data.data.map((art) => (
+        {
+            id: art.id,
+            title: art.title,
+            artist:art.artist_title,
+            image:`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`,
+            description: art.credit_line,
+            century:art.date_display,
+            placeOfOrigin:art.place_of_origin,
+        }));
+    res.json(artData);
+  } catch (e) {
+    next(`Home Route: ${e}`);
+  }
+});
 
 module.exports = Router;
 
-// HOME ('/')
 // FAVORITE ART ('/favlist')
+// all credits go to naser for finding the API
