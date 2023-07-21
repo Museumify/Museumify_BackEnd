66d096cfaa658062d2fc9e1ec221bcada7fd32fc
balqees <balqeesalqudah97@gmail.com>
2023-07-21 11:00:51 +0300

    server-setup-done

---
+++ routes/art.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const Router = express.Router();
+
+module.exports = Router;
+
+// view Card ('/card/:id')
+// get/post/delete/ update
---
+++ routes/general.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const Router = express.Router();
+const axios = require('axios');
+
+module.exports = Router;
+
+// HOME ('/')
+// FAVORITE ART ('/favlist')
