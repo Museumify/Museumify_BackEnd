effcaac6220d31ce95aaca80be8085fc2699cfe1
Reneh7 <renehm2001@gmail.com>
2023-07-22 16:20:00 +0300

    reneh-post-method-done2

--- DataFiltering/idFiltering.js
+++
@@ -1,9 +0,0 @@
-function filterArtDataById(artData, id) {
-    if (!id) {
-      return artData;
-    }
-  
-    return artData.filter((art) => art.id === id);
-  }
-  
-  module.exports = filterArtDataById;
